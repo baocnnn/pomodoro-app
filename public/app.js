a2f1a20f813d86f0166841a1ad9973c46e384690
baocnnn <chris.pascarella@hotmail.com>
2025-10-07 00:03:36 -0500

    added pause and restart button, also added audio on timer completion

--- public/app.js
+++ public/app.js
@@ -1,15 +1,28 @@
 const bells = new Audio('./sounds/bell.wav');
 const startBtn = document.querySelector('.btn-start');
+const pauseBtn = document.querySelector('#pauseBtn');
+const restartBtn = document.querySelector('#restartBtn');
 const session = document.querySelector('.minutes');
 let myInterval;
-let state = true;
+let state = false;
+let isPaused = false;
+let totalSeconds;
+let initialMinutes = 25;
 
 const appTimer = () => {
-    const sessionAmount = Number.parseInt(session.textContent)
+        if(state) {
+            alert("session has already started!");
+            return;
+        }    
 
-    if(state) {
-        state = false;
-        let totalSeconds = sessionAmount * 60;
+        if(!isPaused) {
+            const sessionAmount = Number.parseInt(session.textContent);
+            totalSeconds = sessionAmount * 60; 
+            initialMinutes = sessionAmount;
+        }
+
+        state = true;
+        isPaused = false;
 
         const updateSeconds = () => {
         const minuteDiv = document.querySelector('.minutes');
@@ -30,32 +43,31 @@ const appTimer = () => {
         if(minutesLeft === 0 && secondsLeft === 0) {
             bells.play();
             clearInterval(myInterval);
+            state = false;
         }
         }
         myInterval = setInterval(updateSeconds, 1000);
-    } else {
-        alert('Session has already started!');
+    
     }
+
+const pauseTimer = () => {
+    clearInterval(myInterval);
+    state = false;
+    isPaused = true;
 }
-startBtn,addEventListener('click', appTimer);
-const updateSeconds = () => {
+
+const restartTimer = () => {
+    clearInterval(myInterval);
+    state = false;
+
     const minuteDiv = document.querySelector('.minutes');
     const secondDiv = document.querySelector('.seconds');
+    minuteDiv.textContent = initialMinutes;
+    secondDiv.textContent = '00';
 
-    totalSeconds--;
-
-    let minutesLeft = Math.floor(totalSeconds/60);
-    let secondsLeft = totalSeconds % 60;
-
-    if(secondsLeft < 10) {
-        secondDiv.textContent = '0' + secondsLeft;
-    } else {
-        secondDiv.textContent = secondsLeft;
-    }
-    minuteDiv.textContent = `${minutesLeft}`;
-
-    if(minutesLeft === 0 && secondsLeft === 0) {
-        bells.play()
-        clearInterval(myInterval);
-    }
+    totalSeconds = initialMinutes * 60;
 }
+
+startBtn.addEventListener('click', appTimer);
+pauseBtn.addEventListener('click', pauseTimer);
+restartBtn.addEventListener('click', restartTimer);
